07f836ff8e4a42640c171a2afeb6c8ed7f5f044c
KOFKOY <1838039390@qq.com>
2025-01-20 10:06:02 +0800

    修改代理

--- replaceUrl.sh
+++ replaceUrl.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # 加速cdn地址
 CDN_JSDELIVR_URL="https://cdn.jsdelivr.net/gh/KOFKOY/TVBox@main"
 CDN_MOEYY_URL="https://github.moeyy.xyz/https://raw.githubusercontent.com/KOFKOY/TVBox/main"
+GP_PROXY="https://gh-proxy.com/https://raw.githubusercontent.com/KOFKOY/TVBox/main"
 
 echo "Starting directory: $SCRIPT_DIR"
 
@@ -14,12 +15,12 @@ find "$SCRIPT_DIR" -type f -name "oktv.json" | while read -r file; do
     echo "Processing file: $file"
     # 在这里添加你的文件处理逻辑，例如替换 GitHub URL
     # 例如：sed -i 's/old-url/new-url/g' "$file"
-    sed -i "s|https://raw.kkgithub.com/2hacc/TVBox/main|$CDN_MOEYY_URL|g" "$file"
+    sed -i "s|https://raw.kkgithub.com/2hacc/TVBox/main|$GP_PROXY|g" "$file"
 done
 
 # 递归遍历目录中的所有文件
 find "$SCRIPT_DIR" -type f -name "h.json" | while read -r file; do
     echo "Processing file: $file"
     # http://ftp6284928.host108.abeiyun.cn/h/xBPQ/精东影业2.json
-    sed -i "s|http://ftp6284928.host108.abeiyun.cn|$CDN_MOEYY_URL|g" "$file"
+    sed -i "s|http://ftp6284928.host108.abeiyun.cn|$GP_PROXY|g" "$file"
 done
